cc7cea76bde6bdbf46bd3d698fd5fed4690aaacd
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2024-12-10 16:01:19 -0300

    Service Layer, component registration

--- src/main/java/com/Aprendizado/curso/resources/UserResource.java
+++ src/main/java/com/Aprendizado/curso/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.Aprendizado.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Aprendizado.curso.entities.User;
+import com.Aprendizado.curso.services.UserService;
 
 // Essa classse vai disponibilizar um recurso web correspondente a entidade User.
 
@@ -14,13 +19,23 @@ import com.Aprendizado.curso.entities.User;
 @RestController
 @RequestMapping(value = "/Users") // caminho users
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 // Método que vai ser um endpoint para acessar os usuários e usaremos o ResponseEntity, pois ele serve para retornar respostas de requisições web
     @GetMapping //serve para indicar que esse método abaixo será um método que responde a requisição do tipo Get do http.
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+				
+		return ResponseEntity.ok().body(list);
 	}
+    
+    @GetMapping(value= "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+    	User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+    }
 
     // Esse é nosso controlador Rest que responde no caminho Users
 }
